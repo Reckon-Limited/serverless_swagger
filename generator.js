01cd9b5aa0e5f03c4d41c1ffcb7395a1ff629e25
Chen Osipov <eobender@gmail.com>
2018-06-13 18:28:12 +0300

    added folder suport for tags inside the swagger

--- generator.js
+++ generator.js
@@ -1,6 +1,8 @@
 "use strict";
 var _ = require("lodash");
 var fs = require("fs");
+var dsk = require('path');
+
 function log(s) {
     console.info(s);
 }
@@ -17,12 +19,20 @@ function definition(handler, url, method) {
     };
 }
 exports.definition = definition;
-function handler(namespace, name) {
+function handler(namespace, name, dir) {
+    var result = name + ".main";
+
     if (namespace == '') {
-        return name + ".main";
+        if(dir)
+            return dsk.join(dir,result);
+        else 
+            return result;
     }
     else {
-        return namespace + "/" + name + ".main";
+        if(dir)
+            return dsk.join(namespace,dir,result);
+        else 
+            return dsk.join(namespace,result);
     }
 }
 exports.handler = handler;
@@ -45,8 +55,19 @@ function src(name) {
     return "\nmodule.exports.main = (event, context, callback) => {\n  const response = {\n    statusCode: 200,\n    body: JSON.stringify({\n      name: '" + name + "'\n    }),\n  };\n  callback(null, response);\n};";
 }
 exports.src = src;
-function writeHandler(path, name) {
-    var file = path + "/" + name + ".js";
+function writeHandler(path, name , dir) {
+    var file = '';  
+
+    if (dir){
+        dir = dsk.join(path,dir);
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
+        }
+        file = dsk.join(dir,name + ".js");
+    } else {
+        file = path + "/" + name + ".js"
+    }   
+
     if (fs.existsSync(file)) {
         log("Function handler exists " + name);
     }
@@ -60,10 +81,12 @@ function generate(swaggerPaths, namespace, outputPath) {
     var functions = {};
     _.each(swaggerPaths, function (path, url) {
         _.each(path, function (options, method) {
+
+            var dir = _.camelCase(options ? options.tags[0] : null);
             var fn = name(url, method);
-            var hn = handler(namespace, fn);
+            var hn = handler(namespace, fn, dir);
             var def = definition(hn, url, method);
-            writeHandler(outputPath, fn);
+            writeHandler(outputPath, fn, dir);
             functions[fn] = def;
         });
     });
